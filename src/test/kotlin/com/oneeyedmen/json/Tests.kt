815781a8ef9dedafe8cde980a36185a5f1ceaa54
Duncan McGregor <duncan@oneeyedmen.com>
2021-06-20 10:48:07 +0100

    Separate test from prod

--- src/test/kotlin/com/oneeyedmen/json/Tests.kt
+++ src/test/kotlin/com/oneeyedmen/json/Tests.kt
@@ -6,31 +6,32 @@ import kotlin.test.assertEquals
 class Tests {
 
     @Test
-    fun test() {
+    fun `converter can convert`() {
         val domain = Domain("fred", 42)
-        assertEquals(JDomain("fred", 42), convert(domain))
+        val converter: (Domain) -> JDomain = createConverter(::JDomain,
+            Domain::name,
+            Domain::count
+        )
+        assertEquals(JDomain("fred", 42), converter(domain))
     }
 
-    private fun convert(domain: Domain): JDomain {
-        val converter: (Domain) -> JDomain = createConverter(::JDomain, Domain::name, Domain::count)
-        return converter(domain)
-    }
+    data class Domain(
+        val name: String,
+        val count: Int
+    )
 
-    private fun <I, O, P1, P2> createConverter(
-        constructor: (P1, P2) -> O,
-        p1: (I) -> P1,
-        p2: (I) -> P2
-    ): (I) -> O = {
-        constructor(p1(it), p2(it))
-    }
+    data class JDomain(
+        val name: String,
+        val count: Int
+    )
 }
 
-data class Domain(
-    val name: String,
-    val count: Int
-)
+fun <I, O, P1, P2> createConverter(
+    constructor: (P1, P2) -> O,
+    p1: (I) -> P1,
+    p2: (I) -> P2
+): (I) -> O = {
+    constructor(p1(it), p2(it))
+}
 
-data class JDomain(
-    val name: String,
-    val count: Int
-)
+
