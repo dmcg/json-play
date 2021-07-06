59d7610e29cdf2afca022a42d20cca4960135fc8
Duncan McGregor <duncan@oneeyedmen.com>
2021-07-06 14:23:10 +0100

    import

--- src/test/kotlin/com/oneeyedmen/json/Tests.kt
+++ src/test/kotlin/com/oneeyedmen/json/Tests.kt
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.node.ObjectNode
 import org.junit.jupiter.api.Test
-import kotlin.reflect.KProperty0
 import kotlin.reflect.KProperty1
 import kotlin.test.assertEquals
 
