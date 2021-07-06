c6bbbf51dc13abc5aae7a58740e8fc87aac4e521
Duncan McGregor <duncan@oneeyedmen.com>
2021-07-06 09:36:48 +0100

    tidy

--- src/test/kotlin/com/oneeyedmen/json/Tests.kt
+++ src/test/kotlin/com/oneeyedmen/json/Tests.kt
@@ -11,30 +11,11 @@ class Tests {
     val objectMapper = ObjectMapper()
 
     @Test
-    fun `converter can convert`() {
-        val domain = Domain("fred", 42)
-        val converter: (Domain) -> JDomain = createConverter(
-            ::JDomain,
-            Domain::name,
-            Domain::count
-        )
-        assertEquals(JDomain("fred", 42), converter(domain))
-    }
-
-    @Test
     fun `to JsonNode`() {
         val domain = Domain("fred", 42)
         val expected: JsonNode = objectMapper.createObjectNode().apply {
             this.put("name", "fred")
             this.put("count", 42)
-        }
-
-        fun <D> jsonString(name: String, extractor: (D) -> String) = { node: ObjectNode, value: D ->
-            node.put(name, extractor(value))
-        }
-
-        fun <D> jsonInt(name: String, extractor: (D) -> Int) = { node: ObjectNode, value: D ->
-            node.put(name, extractor(value))
         }
 
         val putters = listOf(
@@ -51,25 +32,22 @@ class Tests {
         assertEquals(expected, converter.toJson(domain))
     }
 
+    fun <D> jsonString(name: String, extractor: (D) -> String) = { node: ObjectNode, value: D ->
+        node.put(name, extractor(value))
+    }
+
+    fun <D> jsonInt(name: String, extractor: (D) -> Int) = { node: ObjectNode, value: D ->
+        node.put(name, extractor(value))
+    }
 
     data class Domain(
         val name: String,
         val count: Int
     )
-
-    data class JDomain(
-        val name: String,
-        val count: Int
-    )
 }
 
 
-fun <I, O, P1, P2> createConverter(
-    constructor: (P1, P2) -> O,
-    p1: (I) -> P1,
-    p2: (I) -> P2
-): (I) -> O = {
-    constructor(p1(it), p2(it))
-}
+
+
 
 
