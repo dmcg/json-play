1462bb1dc88b4875d6c7b7bdfb81884eed988e30
Duncan McGregor <duncan@oneeyedmen.com>
2021-07-07 08:15:54 +0100

    split the tests

---
+++ src/test/kotlin/com/oneeyedmen/json/SchemaTests.kt
@@ -0,0 +1,79 @@
+package com.oneeyedmen.json
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class SchemaTests {
+
+    private val objectMapper = ObjectMapper()
+
+    private data class Parent(
+        val aString: String,
+        val thing: Domain
+    )
+
+    private data class Domain(
+        val name: String,
+        val count: Int
+    )
+
+    @Test
+    fun `simple schema`() {
+        val mapping = jsonMapping(
+            ::Domain,
+            stringProp("the-name", Domain::name),
+            intProp(Domain::count),
+        )
+
+        assertEquals("""
+            {
+                "type" : "object",
+                "properties" : {
+                    "the-name" : { "type" : "string" },
+                    "count" : { "type" : "number" }
+                }
+            }""".toJson(objectMapper),
+            mapping.schema(objectMapper.asNodeFactory())
+        )
+    }
+
+    @Test
+    fun `nested schema`() {
+        val mapping = jsonMapping(
+            ::Parent,
+            prop(Parent::aString),
+            prop(
+                "child",
+                Parent::thing,
+                jsonMapping(
+                    ::Domain,
+                    prop("the-name", Domain::name),
+                    prop(Domain::count),
+                )
+            ),
+        )
+
+        assertEquals("""
+            {
+                "type" : "object",
+                "properties" : {
+                    "aString" : { "type" : "string" },
+                    "child" : {
+                        "type" : "object",
+                        "properties" : {
+                            "the-name" : { "type" : "string" },
+                            "count" : { "type" : "number" }
+                        }
+                    }
+                }
+            }""".toJson(objectMapper),
+            mapping.schema(objectMapper.asNodeFactory())
+        )
+    }
+}
+
+
+
+
+
--- src/test/kotlin/com/oneeyedmen/json/Tests.kt
+++ src/test/kotlin/com/oneeyedmen/json/Tests.kt
@@ -7,47 +7,61 @@ import kotlin.test.assertEquals
 
 class Tests {
 
-    val objectMapper = ObjectMapper()
+    private val objectMapper = ObjectMapper()
+
+    private data class Domain(
+        val name: String,
+        val count: Int
+    ) {
+        constructor() : this("fred", 42) // check we can pick the right ctor
+    }
+
+    private data class Parent(
+        val aString: String,
+        val thing: Domain
+    )
 
     @Test
     fun `simple round trip`() {
         val domain = Domain("fred", 42)
-        val expectedJson = objectNode().apply {
-            put("the-name", "fred")
-            put("count", 42)
-        }
+        val expectedJson = """
+        {
+            "the-name" : "fred",
+            "count" : 42
+        }""".toJson(objectMapper)
 
         val converter = jsonMapping(
             ::Domain,
             stringProp("the-name", Domain::name),
             intProp(Domain::count),
         )
-        assertEquals(expectedJson, converter.toJson(domain, objectMapper.asNodeFactory()))
+        assertEquals(
+            expectedJson,
+            converter.toJson(domain, objectMapper.asNodeFactory())
+        )
         assertEquals(domain, converter.fromJson(expectedJson))
     }
 
     @Test
     fun `nested round trip`() {
-        val domain = Composite(
+        val domain = Parent(
             "banana",
             Domain("fred", 42)
         )
-        val expectedJson = objectNode().apply {
-            put("aString", "banana")
-            set<JsonNode>("child",
-                objectNode().apply {
-                    put("the-name", "fred")
-                    put("count", 42)
-                }
-            )
-        }
+        val expectedJson = """{
+            "aString" : "banana",
+            "child" : {
+                "the-name" : "fred",
+                "count" : 42
+            }
+        }""".toJson(objectMapper)
 
         val converter = jsonMapping(
-            ::Composite,
-            prop(Composite::aString),
+            ::Parent,
+            prop(Parent::aString),
             prop(
                 "child",
-                Composite::thing,
+                Parent::thing,
                 jsonMapping(
                     ::Domain,
                     prop("the-name", Domain::name),
@@ -62,78 +76,8 @@ class Tests {
 
         assertEquals(domain, converter.fromJson(expectedJson))
     }
-
-    @Test
-    fun `simple schema`() {
-        val converter = jsonMapping(
-            ::Domain,
-            stringProp("the-name", Domain::name),
-            intProp(Domain::count),
-        )
-
-        assertEquals("""
-            {
-                "type" : "object",
-                "properties" : {
-                    "the-name" : { "type" : "string" },
-                    "count" : { "type" : "number" }
-                }
-            }""".toJson(),
-            converter.schema(objectMapper.asNodeFactory())
-        )
-    }
-
-    @Test
-    fun `nested schema`() {
-        val converter = jsonMapping(
-            ::Composite,
-            prop(Composite::aString),
-            prop(
-                "child",
-                Composite::thing,
-                jsonMapping(
-                    ::Domain,
-                    prop("the-name", Domain::name),
-                    prop(Domain::count),
-                )
-            ),
-        )
-
-        assertEquals("""
-            {
-                "type" : "object",
-                "properties" : {
-                    "aString" : { "type" : "string" },
-                    "child" : {
-                        "type" : "object",
-                        "properties" : {
-                            "the-name" : { "type" : "string" },
-                            "count" : { "type" : "number" }
-                        }
-                    }
-                }
-            }""".toJson(),
-            converter.schema(objectMapper.asNodeFactory())
-        )
-    }
-
-    fun objectNode() = objectMapper.createObjectNode()
-
-    fun String.toJson() = objectMapper.readTree(this.trimIndent())
 }
 
-data class Domain(
-    val name: String,
-    val count: Int
-) {
-    constructor() : this("fred", 42)
-}
-
-data class Composite(
-    val aString: String,
-    val thing: Domain
-)
-
-
-
+fun String.toJson(objectMapper: ObjectMapper): JsonNode =
+    objectMapper.readTree(this.trimIndent())
 
